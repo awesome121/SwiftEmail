52ba87fffab4c97cbdd18cc7dd5d6a0e93789143
Changxing <jimmy75792@gmail.com>
2021-02-21 00:24:06 +1300

    UI for dealing with template - There is an layout problem - Need to figure out how to hide delete button

--- swiftEmail.py
+++ swiftEmail.py
@@ -1,4 +1,4 @@
-"""A program to send email         Jimmy              23/1/2019
+"""A program to send email         Changxing, Patrick            21/02/2021
 
    Support server: outlook, gmail, qq-email
    
@@ -8,6 +8,7 @@
 """
 from tkinter import *
 from tkinter.ttk import *
+from fileHandler import *
 from smtplib import SMTP
 from email.mime.text import MIMEText
 import smtplib
@@ -16,9 +17,9 @@ from time import asctime
 
 class EmailGui:
     """To send email quicklier with a template"""
-    global ACCOUNT_DICT, RECEIVER_DICT
     def __init__(self, window):
         """Initializer"""
+        self.window = window
         #Labels:
         self.prompt_label = Label(window, text='Welcome to use swift email system!')
         self.prompt_label.grid(row=0, column=0, columnspan=3)      
@@ -41,22 +42,33 @@ class EmailGui:
         #Buttons:
         self.account_enter = Button(window, text='Log In', command=self.connection)
         self.account_enter.grid(row=3, column=1, columnspan=3, sticky=(E, W))
+        
+        self.template_delete = Button(window, text='Delete', command=self.on_click_delete_template)
+        self.template_delete.grid(row=8, column=0)        
+        
+        self.new_template = Button(window, text='New template', command=self.on_click_new_template)
+        self.new_template.grid(row=6, column=2)
            
         self.content_submit = Button(window, text='Send', command=self.send)
         self.content_submit.grid(row=8, column=2)
         
+        
         #Combobox:
-        self.account_combo = Combobox(window, values=list(ACCOUNT_DICT.keys()))
+        self.account_combo = Combobox(window, values=list(get_account_dictionary().keys()))
         self.account_combo.grid(row=1, column=1, sticky=(E, W), columnspan=2)
         
-        self.address_combo = Combobox(window, values=list(RECEIVER_DICT.keys()))
+        self.address_combo = Combobox(window, values=list(get_receiver_dictionary().keys()))
         self.address_combo.grid(row=4, column=1, sticky=(E, W))
+        
+        self.template_combo = Combobox(window, values=list(get_mail_templates(self.account_combo.get()).keys()))
+        self.template_combo.grid(row=7, column=0)
+        self.template_combo.bind('<<ComboboxSelected>>', self.on_selected_combo)
         
         #Entry:
 
         self.password_entry = Entry(window, show='*')
         self.password_entry.grid(row=2, column=1, sticky=(E, W), columnspan=2)
-                
+    
         self.subject_entry = Entry(window)
         self.subject_entry.grid(row=5, column=1, sticky=(E, W))
         self.subject_entry.insert('1', '')
@@ -75,7 +87,7 @@ class EmailGui:
             
         else:
             try:
-                self.account = ACCOUNT_DICT[self.account_combo.get()]
+                self.account = get_account_dictionary()[self.account_combo.get()]
             except KeyError:
                 self.account = self.account_combo.get()
             try:
@@ -102,12 +114,36 @@ class EmailGui:
                 else:
                     self.prompt_label['text'] = self.account +'       Connected    ' + 'Connection made on: ' + time
         
+    def on_click_new_template(self):
+        self.new_template.configure(text='Save', command=self.on_click_save_template)
+        self.prompt_label['text'] = 'Please enter template name and content'
+        self.content.delete(1.0, "end")
+        self.content.insert(1.0, 'Template Name: ""\nTemplate Content: \n"\n\n\n"')
+        
+    def on_click_save_template(self):
+        template_name, template_content = process_text_for_template(self.content.get('1.0', 'end'))
+        save_template(self.account_combo.get(), template_name, template_content, True)
+        self.new_template.configure(text='New template', command=self.on_click_new_template)
+        self.prompt_label['text'] = 'Templated Saved'
+        
+    def on_click_delete_template(self):
+        delete_template(self.account_combo.get(), self.selected_combo_item)
+        self.prompt_label['text'] = 'Template Deleted'
+        #self.template_delete
+        
+    def on_selected_combo(self, event):
+        self.selected_combo_item = event.widget.get()
+        template_content = get_mail_templates(self.account_combo.get())[self.selected_combo_item]
+        self.content.delete(1.0, 'end')
+        self.content.insert(1.0, template_content)
+        
+        
         
     def send(self):
         """Send message"""
         time = asctime().split()[3]
         try:
-            address = RECEIVER_DICT[self.address_combo.get()]
+            address = get_receiver_dictionary[self.address_combo.get()]
         except KeyError:
             address = self.address_combo.get()  
         try:
@@ -122,6 +158,18 @@ class EmailGui:
             self.prompt_label['text'] = 'Send to:  ' + address + '    Successfullly    ' + 'Time: ' + time
 
 #================================================================================
+def process_text_for_template(text):
+    temp_name, temp_content = text[:17], ''
+    length = len(text)
+    i = 0
+    while i < length:
+        if i > 20:
+            if text[i-20:i] == 'Template Content: \n"':
+                temp_content += text[i:-1]
+                break
+        i += 1
+    return temp_name, temp_content
+
 
 def main():
     """Main function"""
